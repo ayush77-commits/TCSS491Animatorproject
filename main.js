6faeec619975bc58ecee0f1c8468a4b487d8b342
ayush77-commits <ayush77@uw.edu>
2026-01-22 11:52:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new ironman());
+	gameEngine.addEntity(new IronMan());
 
 	gameEngine.init(ctx);
 
