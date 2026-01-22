5d06fde8738f847822ab9537e99a51b47376bcef
ayush <ayush77@uw.edu>
2026-01-22 11:38:15 -0800

    adding to git

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./ironman.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+
+	gameEngine.addEntity(new Ironman());
 
 	gameEngine.init(ctx);
 
