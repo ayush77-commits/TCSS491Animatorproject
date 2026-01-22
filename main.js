374a6dedc3e6197a2889810a5227f186c40fd13b
ayush <ayush77@uw.edu>
2026-01-22 11:47:46 -0800

    adding ironman.js and ironman.png to code

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Ironman());
+	gameEngine.addEntity(new ironman());
 
 	gameEngine.init(ctx);
 
